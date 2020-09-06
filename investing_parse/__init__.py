bf6ac409b00812c28eebd3a622854e6b48aa853d
Egrassa <bacek90@mail.ru>
2020-09-06 22:29:07 +0500

    добавил проверку на мак систему и выброс исключения

--- investing_parse/__init__.py
+++ investing_parse/__init__.py
@@ -3,12 +3,15 @@ import os
 BASE_DIR = os.path.join(os.getcwd(), 'investing_parse')
 REPORT_DIR_PATH = os.path.join(BASE_DIR, 'report')
 SCREENSHOT_DIR_PATH = os.path.join(BASE_DIR, 'screenshots')
+
 if os.name == 'nt':
     DRIVER_DIR_PATH = os.path.join(BASE_DIR, 'browser_webdriver', 'nt')
 elif os.name == 'posix':
     DRIVER_DIR_PATH = os.path.join(BASE_DIR, 'browser_webdriver', 'linux')
+elif os.name == 'mac':
+    DRIVER_DIR_PATH = os.path.join(BASE_DIR, 'browser_webdriver', 'mac')
 else:
-    DRIVER_DIR_PATH = os.path.join(BASE_DIR, 'browser_webdriver')
+    raise Exception('Под такую систему нет драйвера')
 
 CHROMEDRIVER_PATH = os.path.join(DRIVER_DIR_PATH, 'chromedriver')
 GECKODRIVER_PATH = os.path.join(DRIVER_DIR_PATH, 'geckodriver')
