90db6de758021d680bcb5ec727a8edd92f8bc7dc
Egrassa <bacek90@mail.ru>
2020-09-18 21:33:19 +0500

    проверка наличия доступа перенесена

--- investing_parse/__init__.py
+++ investing_parse/__init__.py
@@ -1,13 +1,5 @@
 import os
 
-
-def check_access(path):
-    if not os.access(path, os.F_OK):
-        raise FileExistsError(f'Файла {path} несуществует')
-    if not os.access(path, os.X_OK):
-        raise PermissionError(f'У файла {path} нет прав на исполнение')
-
-
 BASE_DIR = os.path.join(os.getcwd(), 'investing_parse')
 REPORT_DIR_PATH = os.path.join(BASE_DIR, 'report')
 SCREENSHOT_DIR_PATH = os.path.join(BASE_DIR, 'screenshots')
@@ -26,6 +18,3 @@ GECKODRIVER_PATH = os.path.join(DRIVER_DIR_PATH, 'geckodriver')
 
 CHROME = 'chrome'
 FIREFOX = 'firefox'
-
-check_access(CHROMEDRIVER_PATH)
-check_access(GECKODRIVER_PATH)
--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,18 @@
 import os
 
-from investing_parse import REPORT_DIR_PATH, BASE_DIR
+from investing_parse import REPORT_DIR_PATH, BASE_DIR, CHROMEDRIVER_PATH, GECKODRIVER_PATH
+
+
+def check_access(path):
+    if not os.access(path, os.F_OK):
+        raise FileExistsError(f'Файла {path} несуществует')
+    if not os.access(path, os.X_OK):
+        raise PermissionError(f'У файла {path} нет прав на исполнение')
 
 
 def main():
+    check_access(CHROMEDRIVER_PATH)
+    check_access(GECKODRIVER_PATH)
     print('Начало тестов behave')
     json_path = os.path.join(REPORT_DIR_PATH, 'behave.json')
     start_dir = os.path.join(BASE_DIR, 'features')
